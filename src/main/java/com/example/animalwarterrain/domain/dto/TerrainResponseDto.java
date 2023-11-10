97b6023225c331cbcc22f9a21c19a447a40e872b
JUNGI JEONG <junejjk@gmail.com>
2023-11-10 14:23:09 +0900

    For merge

--- src/main/java/com/example/animalwarterrain/domain/dto/TerrainResponseDto.java
+++ src/main/java/com/example/animalwarterrain/domain/dto/TerrainResponseDto.java
@@ -1,16 +1,14 @@
 package com.example.animalwarterrain.domain.dto;
 
 import com.example.animalwarterrain.domain.entity.LandForm;
-import lombok.AllArgsConstructor;
-import lombok.Data;
 
 import java.util.UUID;
 
-
 public record TerrainResponseDto(
- UUID userUUID,
- LandForm landForm
-
-        ) {
-
+        UUID userUUID,
+        LandForm dominantLandForm,
+        int sea,
+        int land,
+        int mountain
+) {
 }
--- src/main/java/com/example/animalwarterrain/domain/entity/Terrain.java
+++ src/main/java/com/example/animalwarterrain/domain/entity/Terrain.java
@@ -25,8 +25,25 @@ public class Terrain {
     @OneToMany(mappedBy = "terrain", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Tile> tiles;
 
+    private int sea;
+    private int land;
+    private int mountain;
+
 
     public void updateDominantLandForm(LandForm dominantLandForm) {
         this.dominantLandForm = dominantLandForm;
     }
+
+    public void updateSeaCount(int sea){
+        this.sea = sea;
+    }
+
+    public void updateLandCount(int land){
+        this.land = land;
+    }
+    public void updateMountainCount(int Mountain){
+        this.mountain = mountain;
+    }
+
+
 }
